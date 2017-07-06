a4686ab7fe9ff174b8526e28ad46df62b55ad245
kyhoolee <kyhoolee@gmail.com>
2017-07-06 16:51:48 +0700

    update

--- src/main/java/com/example/rest/Controller.java
+++ src/main/java/com/example/rest/Controller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +26,7 @@ public class Controller {
 	}
 
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	 @PostMapping(value = "/")
 	public ResponseEntity<Car> update(@RequestBody Car car) {
 
 	    if (car != null) {
